c093b0d9a5abf274008502d34fcfb0144afc0b74
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-17 18:24:41 +0100

    User can create team

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,9 +24,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Team
+    Route::resource('teams', TeamController::class);
+
 });
 
 require __DIR__.'/auth.php';
--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Team;
 use App\Models\User;
 use Database\Factories\UserFactory;
 use Illuminate\Foundation\Auth\User as AuthUser;
@@ -11,25 +12,32 @@ use Tests\TestCase;
 
 class TeamTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase,WithFaker;
 
     /**
      * Test if a User can create a Team.
      *
      * @return void
      */
-    public function testCreateTeam()
+    public function testUserCanCreateATeam()
     {
         $user = User::Factory()->create();
+        $teamName = $this->faker()->uuid();
 
-        $this->actingAs($user)
-            ->post('/teams', [
-                'name' => 'Test Team'
-            ])
-            ->assertOk()
-            ->seeJson([
-                'success' => true
-            ]);
+        $response = $this
+        ->actingAs($user)
+        ->post('/teams', [
+            'name' => $teamName
+        ]);
+
+        $response
+        ->assertSessionHasNoErrors()
+        ->assertRedirect();
+
+        // Assert $user owns team
+        $team = Team::where(['user_id'=>$user->id,'name'=>$teamName])->first();
+        $this->assertNotEmpty($team);
+
 
 
     }
