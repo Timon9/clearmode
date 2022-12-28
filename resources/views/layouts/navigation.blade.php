6f0a2b81779ea062e5aa16e22497bbd4e31da9cb
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-28 12:28:53 +0100

    WIP: Team front end

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('team.index')" :active="request()->routeIs('team')">
+                        {{ __('Teams') }}
+                    </x-nav-link>
                 </div>
             </div>
 
