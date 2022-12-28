19e1b6af0e2d691a359491a18ac27aedf666f47d
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-28 12:12:59 +0100

    Add comments

--- app/Models/TeamInvite.php
+++ app/Models/TeamInvite.php
@@ -2,19 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Import the HasFactory trait
+use Illuminate\Database\Eloquent\Model; // Import the Model class
 
 class TeamInvite extends Model
 {
-    use HasFactory;
+    use HasFactory; // Use the HasFactory trait
 
-
+    /**
+     * Define a one-to-one relationship with a Team model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function team()
     {
         return $this->belongsTo(Team::class);
     }
 
+    /**
+     * Define a one-to-one relationship with a User model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
