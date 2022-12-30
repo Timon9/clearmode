cf5cadd8209bc297eba1026700161d8b77912f36
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-30 13:51:14 +0100

    Unit test for initials

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -26,4 +26,26 @@ class UserTest extends TestCase
         $this->assertIsString($avatar);
         $this->assertStringStartsWith('data:image/png;base64,', $avatar);
     }
+
+    /**
+     * Test the getInitialsAttribute function.
+     *
+     * @return void
+     */
+    public function testWeCanGenerateInitials()
+    {
+        $user = new User;
+
+        // Test with two words
+        $user->name = 'John Smith';
+        $this->assertEquals('JS', $user->getInitialsAttribute());
+
+        // Test with one word
+        $user->name = 'John';
+        $this->assertEquals('Jo', $user->getInitialsAttribute());
+
+        // Test with three words
+        $user->name = 'John Adam Smith';
+        $this->assertEquals('JA', $user->getInitialsAttribute());
+    }
 }
