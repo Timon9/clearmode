fdd0d2e9ed2cae2ed3c5542c5d94c28835bc22ec
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-12-30 21:49:12 +0100

    cleanup

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -18,10 +18,10 @@
 
                         <div class="mb-4 py-4 flex">
                             <!-- Start: New team -->
-                                <x-primary-link href="{{ route('teams.create') }}" class="mr-4 mt-1 h-10">
-                                    <i class="fas fa-plus mr-2"></i>{{ __('New team') }}
-                                </x-primary-link>
-
+                            <x-primary-link href="{{ route('teams.create') }}" class="mr-4 mt-1 h-10">
+                                <i class="fas fa-plus mr-2"></i>{{ __('New team') }}
+                            </x-primary-link>
+                            <!-- End: New team -->
 
                             <!-- Start: Search -->
                             <form action="{{ route('teams.index') }}" method="GET" class="flex items-center w-3/4">
